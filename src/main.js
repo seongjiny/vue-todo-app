2f0834c98ce6b2ec16937632f64944f3d59d0def
Seongjin Yoon <seongjin.yoon@paiptree.com>
2024-07-06 14:47:27 +0900

    APIKEY 가리고 재 업로드

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import {createApp} from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY ?? '',
+  authDomain: "crew-manager-e370a.firebaseapp.com",
+  databaseURL:
+    "https://crew-manager-e370a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crew-manager-e370a",
+  storageBucket: "crew-manager-e370a.appspot.com",
+  messagingSenderId: "121632428862",
+  appId: "1:121632428862:web:14fd3e791be23d12a1526f",
+};
+firebase.initializeApp(firebaseConfig);
+
+const app = createApp(App);
+app.mount("#app");
